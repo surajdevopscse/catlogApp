da7caa177724b2bef1ed04cf28752625d0b1ebf5
unknown <ashutoshay2505@gmail.com>
2021-06-18 19:37:01 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/catlogapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.catlogapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Catlog App'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text('The Catlog App'),
+      ),
+    ),
+  ));
+}
